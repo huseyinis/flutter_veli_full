762873b812ed44fd1c7c8e7a100038e189e683db
huseyin <fzt.hsyns@gmail.com>
2023-03-16 12:35:52 +0300

    atomic etc

--- lib/demos/password_text_field.dart
+++ lib/demos/password_text_field.dart
@@ -1,21 +1,48 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
-class PasswordTextField extends StatelessWidget {
-  const PasswordTextField({super.key});
+class PasswordTextField extends StatefulWidget {
+  const PasswordTextField({Key? key, required this.controller}) : super(key: key);
+  final TextEditingController controller;
+  @override
+  State<PasswordTextField> createState() => _PasswordTextFieldState();
+}
+
+class _PasswordTextFieldState extends State<PasswordTextField> {
   final _obsucureText = '#';
+  bool _isSecure = true;
+
+  void _changeLoading() {
+    setState(() {
+      _isSecure = !_isSecure;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: widget.controller,
       autofillHints: [AutofillHints.password],
       keyboardType: TextInputType.visiblePassword,
 
       //password ıslemlerınde obsocuretext kullanılırsa yazılan text gorunmuyor
       // sıfre gırme ıslemlerı ıcın oldukca ıyı
-      obscureText: true,
+      obscureText: _isSecure,
       //gızlerken hangı karakter kullanılsın onu ayarlamamızı saglar
       obscuringCharacter: _obsucureText,
-      decoration: InputDecoration(border: UnderlineInputBorder(), hintText: 'Password'),
+      decoration: InputDecoration(
+        border: UnderlineInputBorder(),
+        hintText: 'Password',
+        suffix: _onVisibityIcon(),
+      ),
     );
+  }
+
+  IconButton _onVisibityIcon() {
+    return IconButton(
+        onPressed: () {
+          _changeLoading();
+        },
+        icon: Icon(_isSecure ? Icons.visibility : Icons.visibility_off));
   }
 }
